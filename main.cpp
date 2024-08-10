cfdd3ea68717caf8293df3f2d99dd2aca29e7b35
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-10 22:40:20 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std ;
+int main()
+{
+    double x ;
+    cin >> x ;
+    int y = x/1 ;
+    double z = x-y;
+    if ("x is float" , x&&z>0)
+    {
+      cout << "float" << " " << y << " " << z << endl ;
+
+    }
+    else
+    {
+       cout << "int" << " " << y << endl ;
+    }
+    return 0;
+}
